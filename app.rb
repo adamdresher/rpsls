dbf2b3c9e3804992d190d0327f2d45817370637f
Adam Dresher <adamdresher@gmail.com>
2023-04-01 17:35:01 +0700

    add flash messages ; change 'user' to 'you'

--- app.rb
+++ app.rb
@@ -26,19 +26,55 @@ configure do
 end
 
 def setup_game
-  # session[:rounds] = {}
-  session[:history] = {} # update references
-  # (1..rounds.to_i).each { |round| session[:rounds][round] = nil }
+  session[:history] = []
   session[:current_round] = 1
 
 
   session[:players] = {}
 
-  [:user, :computer].each do |player|
+  [:you, :computer].each do |player|
     session[:players][player] = {}
     session[:players][player][:moves] = []
     session[:players][player][:score] = 0
   end
+end
+
+def computer_move
+  VALID_CHOICES.sample
+end
+
+def current_round_description
+  user_move = session[:players][:you][:moves].last
+  computer_move = session[:players][:computer][:moves].last
+
+  RPSLS.each do |str|
+    words = str.downcase.split
+    return "It's a tie!" if user_move == computer_move
+    return str if words.count(user_move) == 1 && words.count(computer_move) == 1
+    # return str if [user_move, computer_move].all? { |player| words.include? player }
+  end
+end
+
+def record_round_moves_and_description!
+  session[:players][:you][:moves] << params[:move]
+  session[:players][:computer][:moves] << computer_move
+  session[:history][session[:current_round]] = current_round_description
+end
+
+def update_scores!(winner)
+  session[:players][winner][:score] += 1
+end
+
+def determine_round_winner
+  user_move = session[:players][:you][:moves].last
+  computer_move = session[:players][:computer][:moves].last
+
+  RPSLS.each do |str|
+    words = str.downcase.split
+    return :you if words.first == user_move && words.last == computer_move 
+    return :computer if words.first == computer_move && words.last == user_move 
+  end
+  nil
 end
 
 def game_winner
@@ -50,49 +86,17 @@ end
 
 def winner?(player)
   player_score = player[:score]
-  # winning_score = (session[:rounds].size / 2) + 1
   winning_score = session[:winner_needs]
 
-  # return false if player_score.zero?
-  # session[:rounds].size.divmod(player_score) == [2, 1]
   player_score >= winning_score
 end
 
-def computer_move
-  VALID_CHOICES.sample
-end
-
-def current_round_description
-  user_move = session[:players][:user][:moves].last
-  computer_move = session[:players][:computer][:moves].last
-
-  RPSLS.each do |str|
-    words = str.downcase.split
-    return "It's a tie!" if user_move == computer_move
-    return str if words.count(user_move) == 1 && words.count(computer_move) == 1
+def winning_message(player, round_or_game)
+  if player == 'you'
+    "You have won the #{round_or_game}!"
+  else
+    "The computer has won the #{round_or_game}!"
   end
-end
-
-def record_round_moves_and_description!
-  session[:players][:user][:moves] << params[:move]
-  session[:players][:computer][:moves] << computer_move
-  session[:history][session[:current_round]] = current_round_description
-end
-
-def determine_round_winner
-  user_move = session[:players][:user][:moves].last
-  computer_move = session[:players][:computer][:moves].last
-
-  RPSLS.each do |str|
-    words = str.downcase.split
-    return :user if words.first == user_move && words.last == computer_move 
-    return :computer if words.first == computer_move && words.last == user_move 
-  end
-  nil
-end
-
-def update_scores!(winner)
-  session[:players][winner][:score] += 1
 end
 
 get '/' do
@@ -111,26 +115,21 @@ get '/play' do
 end
 
 post '/play' do
-  # add user's move choice to records
-  # select random choice for computer
-  # add computer's choice to records
-
-  # evaluate round's winner
   record_round_moves_and_description!
   session[:current_round] += 1
   winner = determine_round_winner
   update_scores!(winner) if winner
   if game_winner
-    # set flash message for game winner
+    session[:message] = [winning_message(game_winner, 'game')]
     redirect '/winner'
   else
-    # set flash message for winner
-    # session[:message] = "#{round_winner} has won this round!"
+    session[:message] = [session[:history].last]
+    session[:message] << "\n#{winning_message(winner, 'round')}" if winner
   end
 
   redirect '/play'
 end
 
 get '/winner' do
-  "Congratulations to the winner!"
+  erb :winner
 end
